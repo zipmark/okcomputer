4373fee93f7a2f35e4fd41526833b4aa0e152786
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-04 13:32:42 -0600

    Implement Checks.registered_check

--- lib/okcomputer/checks.rb
+++ lib/okcomputer/checks.rb
@@ -1,5 +1,34 @@
 module OKComputer
   class Checks
+    # Public: The check registered to the given name
+    #
+    # check_name - The name of the check to retrieve
+    #
+    # Returns the registered check or raises Checks::CheckNotFound
+    def self.registered_check(check_name)
+      (@registered_checks || {}).fetch(check_name)
+    rescue KeyError
+      raise CheckNotFound
+    end
 
+    # Private: The list of checks registered to the system
+    #
+    # Returns an Array of registered checks
+    def self.registered_checks
+      (@registered_checks || {}).values
+    end
+    private_class_method :registered_checks
+
+    # Private: Store a new list of checks
+    #
+    # checks - A Hash with the name of the check as the key and the check for
+    #   that name as the value
+    def self.registered_checks=(checks)
+      @registered_checks = checks
+    end
+    private_class_method :registered_checks=
+
+    # used when fetching a check that has not been registered
+    CheckNotFound = Class.new(StandardError)
   end
 end
---
+++ spec/okcomputer/checks_spec.rb
@@ -0,0 +1,36 @@
+require "spec_helper"
+
+module OKComputer
+  describe Checks do
+    context ".registered_checks" do
+      let(:no_checks) { nil }
+      let(:some_checks) { {foo: "bar"} }
+
+      it "returns an empty list if not set" do
+        Checks.send(:registered_checks=, no_checks)
+        Checks.send(:registered_checks).should == []
+      end
+
+      it "remembers the checks given to it" do
+        Checks.send(:registered_checks=, some_checks)
+        Checks.send(:registered_checks).should == some_checks.values
+      end
+    end
+
+    context ".registered_check(check_name)" do
+      let(:check_name) { :foo }
+      let(:foo_check) { stub(:checker) }
+
+      it "returns the check registered with the given name" do
+        Checks.send(:registered_checks=, {check_name => foo_check})
+        Checks.registered_check(check_name).should == foo_check
+      end
+
+      it "raises an exceiption if given a check that's not registered" do
+        Checks.send(:registered_checks=, {})
+        expect { Checks.registered_check(check_name) }.to raise_error(Checks::CheckNotFound)
+      end
+    end
+  end
+end
+
