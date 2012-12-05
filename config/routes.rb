71f5543259bebf83c6c1cacdae8ec426e76c038e
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-05 14:50:31 -0600

    start defining the engine's routes correctly for a Rails 3.X app

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-OKComputer::Engine.routes.draw do
-  match "/" => "ok_computer#show", defaults: {check: "default", format: "text"}
-  match "/all(.:format)" => "ok_computer#index", defaults: {format: "text"}
-  match "/(:check)(.:format)" => "ok_computer#show", defaults: {format: "text"}
+Rails.application.routes.draw do
+  match "/okcomputer" => "ok_computer#show", defaults: {check: "default", format: "text"}
+  match "/okcomputer/all(.:format)" => "ok_computer#index", defaults: {format: "text"}
+  match "/okcomputer/(:check)(.:format)" => "ok_computer#show", defaults: {format: "text"}
 end
