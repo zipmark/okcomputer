ae9411fb67805f100bf9710d6ef54f8d96b1b83f
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-05 13:11:20 -0600

    remove outer wrapper to registering a checker
    Because it makes @anfleene cry.

--- lib/okcomputer.rb
+++ lib/okcomputer.rb
@@ -6,14 +6,7 @@ require "okcomputer/registry"
 require "okcomputer/built_in_checks/default_check"
 
 module OKComputer
-  # Public: Register the given check with the given name
-  #
-  # check_name - Unique name to give the check
-  # check_object - Instance of Checker to register
-  def self.register(check_name, check_object)
-    Registry.register(check_name, check_object)
-  end
 end
 
-OKComputer.register "default", OKComputer::DefaultCheck.new
+OKComputer::Registry.register "default", OKComputer::DefaultCheck.new
 
