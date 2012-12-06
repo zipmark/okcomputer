9912d6cda50747bb30fd38837973e57b4a34ea36
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-06 14:18:54 -0600

    remove some duplication to determine HTTP status code

--- app/controllers/ok_computer_controller.rb
+++ app/controllers/ok_computer_controller.rb
@@ -5,12 +5,17 @@ class OkComputerController < ApplicationController
   def index
     checks = OKComputer::Registry.all
 
-    respond_with checks, status: (checks.success? ? :ok : :error)
+    respond_with checks, status: status_code(checks)
   end
 
   def show
     check = OKComputer::Registry.fetch(params[:check])
 
-    respond_with check, status: (check.success? ? :ok : :error)
+    respond_with check, status: status_code(check)
   end
+
+  def status_code(check)
+    check.success? ? :ok : :error
+  end
+  private :status_code
 end
