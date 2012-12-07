e794a59ba102c3d743a2cab05be21337fd9c02cf
Patrick Byrne <patrick.byrne@tstmedia.com>
2012-12-07 14:44:16 -0600

    move database_check to active_record_check

--- lib/okcomputer.rb
+++ lib/okcomputer.rb
@@ -4,11 +4,11 @@ require "okcomputer/check_collection"
 require "okcomputer/registry"
 
 # and the built-in checks
+require "okcomputer/built_in_checks/active_record_check"
 require "okcomputer/built_in_checks/default_check"
-require "okcomputer/built_in_checks/database_check"
+require "okcomputer/built_in_checks/mongoid_check"
 require "okcomputer/built_in_checks/resque_backed_up_check"
 require "okcomputer/built_in_checks/resque_down_check"
-require "okcomputer/built_in_checks/mongoid_check"
 
 module OKComputer
 end
