cbc31401077aa3e25797f433a4619696e089ddce
Ian Ehlert <ian.ehlert@tstmedia.com>
2012-12-07 14:14:38 -0600

    Added mongoid check

--- lib/okcomputer.rb
+++ lib/okcomputer.rb
@@ -8,6 +8,7 @@ require "okcomputer/built_in_checks/default_check"
 require "okcomputer/built_in_checks/database_check"
 require "okcomputer/built_in_checks/resque_backed_up_check"
 require "okcomputer/built_in_checks/resque_down_check"
+require "okcomputer/built_in_checks/mongoid_check"
 
 module OKComputer
 end
