4eeefb469f3252431a4f5b2e50188b61c4a6deb7
Patrick Byrne <patrick.byrne@tstmedia.com>
2013-07-01 10:06:34 -0500

    Switch from `gemfile:` to `env:` to Switch Rails
    This is how rspec-rails does it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  match "/okcomputer" => "ok_computer#show", defaults: {check: "default"}
-  match "/okcomputer/all" => "ok_computer#index"
-  match "/okcomputer/:check" => "ok_computer#show"
+  get "/okcomputer" => "ok_computer#show", defaults: {check: "default"}
+  get "/okcomputer/all" => "ok_computer#index"
+  get "/okcomputer/:check" => "ok_computer#show"
 end
