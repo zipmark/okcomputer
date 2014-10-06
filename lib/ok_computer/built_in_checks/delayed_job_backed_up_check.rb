f3d34d0fb96349b79ed775c487ff0ef3fae8184a
Elliot Hursh <elliothursh@gmail.com>
2014-10-06 16:14:31 -0500

    changed delayed job backed up to allow for greater than

--- lib/ok_computer/built_in_checks/delayed_job_backed_up_check.rb
+++ lib/ok_computer/built_in_checks/delayed_job_backed_up_check.rb
@@ -2,10 +2,11 @@ module OkComputer
   class DelayedJobBackedUpCheck < SizeThresholdCheck
     attr_accessor :priority
     attr_accessor :threshold
+    attr_accessor :greater_than_priority
 
     # Public: Initialize a check for backed-up Delayed Job jobs
     #
-    # priority - Which priority (or greater) to check for
+    # priority - Which priority to check for
     # threshold - An Integer to compare the jobs count against
     #   to consider it backed up
     #
@@ -14,18 +15,27 @@ module OkComputer
     #   # => The check will look for jobs with priority between
     #   # 0 and 10, considering the jobs as backed up if there
     #   # are more than 50 of them
-    def initialize(priority, threshold)
+    def initialize(priority, threshold, options = {})
       self.priority = Integer(priority)
       self.threshold = Integer(threshold)
-      self.name = "Delayed Jobs within priority '#{priority}'"
+      self.greater_than_priority = options[:greater_than_priority].nil? ? false : options[:greater_than_priority]
+      self.name = greater_than_priority ? "Delayed Jobs with priority higher than '#{priority}'" : "Delayed Jobs with priority lower than '#{priority}'"
     end
 
     # Public: How many delayed jobs are pending within the given priority
     def size
       if defined?(::Delayed::Backend::Mongoid::Job) && Delayed::Worker.backend == Delayed::Backend::Mongoid::Job
-        Delayed::Job.lte(priority: priority).where(:locked_at => nil, :last_error => nil).count
+        if greater_than_priority
+          Delayed::Job.gte(priority: priority).where(:locked_at => nil, :last_error => nil).count
+        else
+          Delayed::Job.lte(priority: priority).where(:locked_at => nil, :last_error => nil).count
+        end
       else
-        Delayed::Job.where("priority <= ?", priority).where(:locked_at => nil, :last_error => nil).count
+        if greater_than_priority
+          Delayed::Job.where("priority >= ?", priority).where(:locked_at => nil, :last_error => nil).count
+        else
+          Delayed::Job.where("priority <= ?", priority).where(:locked_at => nil, :last_error => nil).count
+        end
       end
     end
   end
--- spec/ok_computer/built_in_checks/delayed_job_backed_up_check_spec.rb
+++ spec/ok_computer/built_in_checks/delayed_job_backed_up_check_spec.rb
@@ -32,6 +32,12 @@ module OkComputer
       end
     end
 
+    context ".new(priority, threshold, :greater_than_priority => true)" do
+      it "accepts an options hash, and assigns variables correctly" do
+        DelayedJobBackedUpCheck.new(priority, threshold, :greater_than_priority => true).greater_than_priority.should == true
+      end
+    end
+
     context "#check" do
       context "when not backed up" do
         before do
@@ -39,7 +45,7 @@ module OkComputer
         end
 
         it { should be_successful }
-        it { should have_message "Delayed Jobs within priority '#{subject.priority}' at reasonable level (#{subject.size})"}
+        it { should have_message "Delayed Jobs with priority lower than '#{subject.priority}' at reasonable level (#{subject.size})"}
       end
 
       context "when backed up" do
@@ -48,7 +54,28 @@ module OkComputer
         end
 
         it { should_not be_successful }
-        it { should have_message "Delayed Jobs within priority '#{subject.priority}' is #{subject.size - subject.threshold} over threshold! (#{subject.size})"}
+        it { should have_message "Delayed Jobs with priority lower than '#{subject.priority}' is #{subject.size - subject.threshold} over threshold! (#{subject.size})"}
+      end
+
+      context "with greater_than_priority == true" do
+        subject { DelayedJobBackedUpCheck.new priority, threshold , :greater_than_priority =>  true }
+        context "when not backed up" do
+          before do
+            subject.stub(:size) { 89 }
+          end
+
+          it { should be_successful }
+          it { should have_message "Delayed Jobs with priority higher than '#{subject.priority}' at reasonable level (#{subject.size})"}
+        end
+
+        context "when backed up" do
+          before do
+            subject.stub(:size) { 123 }
+          end
+
+          it { should_not be_successful }
+          it { should have_message "Delayed Jobs with priority higher than '#{subject.priority}' is #{subject.size - subject.threshold} over threshold! (#{subject.size})"}
+        end
       end
     end
 
@@ -76,6 +103,34 @@ module OkComputer
           Delayed::Job.should_receive(:where).with(:locked_at => nil, :last_error => nil).and_return(Delayed::Job)
           Delayed::Job.should_receive(:count).with(no_args()).and_return(456)
           subject.size.should eq 456
+        end
+      end
+
+      context "with greater_than_priority == true" do
+        subject { DelayedJobBackedUpCheck.new priority, threshold , :greater_than_priority =>  true }
+        context "when Mongoid defined" do
+          before do
+            stub_const('Delayed::Backend::Mongoid::Job', Object.new)
+            stub_const('Delayed::Worker', Object.new).should_receive(:backend).and_return(Delayed::Backend::Mongoid::Job)
+          end
+
+          it "checks Delayed::Job's count of pending jobs within the given priority" do
+            Delayed::Job.should_receive(:gte).with(priority: priority).and_return(Delayed::Job)
+            Delayed::Job.should_receive(:where).with(:locked_at => nil, :last_error => nil).and_return(Delayed::Job)
+            Delayed::Job.should_receive(:count).with(no_args()).and_return(456)
+            subject.size.should eq 456
+          end
+        end
+
+        context "when Mongoid not defined" do
+          before { hide_const 'Delayed::Backend::Mongoid::Job' }
+
+          it "checks Delayed::Job's count of pending jobs within the given priority" do
+            Delayed::Job.should_receive(:where).with("priority >= ?", priority).and_return(Delayed::Job)
+            Delayed::Job.should_receive(:where).with(:locked_at => nil, :last_error => nil).and_return(Delayed::Job)
+            Delayed::Job.should_receive(:count).with(no_args()).and_return(456)
+            subject.size.should eq 456
+          end
         end
       end
     end
