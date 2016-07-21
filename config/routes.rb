f2f5420670ab61c063b0561fc14b6363251a8146
Chris Arcand <chris@chrisarcand.com>
2016-07-21 16:39:05 -0500

    Use alternate default parameters for #root
    There is a regression in Rails 5 detailed in
    https://github.com/sportngin/okcomputer/issues/98
    
    This alternate syntax does work, and therefore allows this release of
    OkComputer to work with the first Rails 5.0 release (and doesn't allow
    the bug to block us)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 OkComputer::Engine.routes.draw do
-  root to: "ok_computer#show", defaults: {check: "default"}, via: [:get, :options]
+  root to: "ok_computer#show", via: [:get, :options], check: "default"
   match "/all" => "ok_computer#index", via: [:get, :options], as: :okcomputer_checks
   match "/:check" => "ok_computer#show", via: [:get, :options], as: :okcomputer_check
 end
