e094b8e95ac5c6710c53452c323cf0e1bcb7623b
Patrick Byrne <code@patrickbyrne.net>
2017-03-24 12:52:50 -0500

    Accept mix of strings and symbols as check names

--- spec/ok_computer/registry_spec.rb
+++ spec/ok_computer/registry_spec.rb
@@ -29,6 +29,12 @@ module OkComputer
       let(:second_check_object) { double(:second_checker, :registrant_name= => nil) }
       let(:default_collection) { double }
 
+      after do
+        # Clear out registered checks to avoid leaking test doubles
+        Registry.instance_variable_defined?(:@default_collection) &&
+          Registry.remove_instance_variable(:@default_collection)
+      end
+
       it "assigns the given name to the check" do
         expect(check_object).to receive(:registrant_name=).with(check_name)
         Registry.register(check_name, check_object)
@@ -49,7 +55,7 @@ module OkComputer
         expect(Registry.registry.values).not_to include check_object
         expect(Registry.registry[check_name]).to eq(second_check_object)
       end
-      
+
       it "uses the default collection if you don't pass a collection name" do
         allow(Registry).to receive(:default_collection){ default_collection }
         expect(default_collection).to receive(:register).with(check_name, check_object)
@@ -65,6 +71,16 @@ module OkComputer
         Registry.register('test_collection', collection)
         Registry.register(check_name, check_object, 'test_collection')
         expect(collection.fetch(check_name)).to eq(check_object)
+      end
+
+      it "gracefully handles checks defined with a combination of strings and symbols as their name" do
+        Registry.register("foo", Check.new)
+        Registry.register(:bar, Check.new)
+
+        result = OkComputer::Registry.all.to_text
+
+        expect(result).to match(/\bfoo\b/)
+        expect(result).to match(/\bbar\b/)
       end
     end
 
